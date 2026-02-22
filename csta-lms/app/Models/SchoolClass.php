9a6bae06df7bf8b3d27f4474fe538002cd588297
sh3ki <shekaigarcia@gmail.com>
2026-02-22 22:16:37 +0800

    Create SchoolClass.php

---
+++ csta-lms/app/Models/SchoolClass.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SchoolClass extends Model
+{
+    protected $table = 'classes';
+
+    protected $fillable = ['name', 'teacher_id'];
+
+    public function teacher()
+    {
+        return $this->belongsTo(User::class, 'teacher_id');
+    }
+
+    public function students()
+    {
+        return $this->belongsToMany(User::class, 'class_student', 'class_id', 'student_id');
+    }
+
+    public function subjects()
+    {
+        return $this->hasMany(Subject::class, 'class_id');
+    }
+}
