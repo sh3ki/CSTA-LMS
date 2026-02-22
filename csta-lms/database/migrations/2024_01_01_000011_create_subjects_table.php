7d53d5744dd4fb94950a44bb498f7721d0733ae7
sh3ki <shekaigarcia@gmail.com>
2026-02-22 22:17:01 +0800

    Create 2024_01_01_000011_create_subjects_table.php

---
+++ csta-lms/database/migrations/2024_01_01_000011_create_subjects_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('class_id')->nullable();
+            $table->foreign('class_id')->references('id')->on('classes')->onDelete('set null');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('subjects');
+    }
+};
