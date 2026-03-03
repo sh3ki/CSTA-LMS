e52054e240b0bcd6cf53477644143428407e5fb3
sh3ki <shekaigarcia@gmail.com>
2026-03-03 11:18:18 +0800

    Create csta-lms/app/Models/Resource.php

---
+++ csta-lms/app/Models/Resource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Resource extends Model
+{
+    protected $fillable = [
+        'subject_id',
+        'title',
+        'description',
+        'file_path',
+        'file_name',
+        'file_type',
+        'uploaded_by',
+    ];
+
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class);
+    }
+
+    public function uploader()
+    {
+        return $this->belongsTo(User::class, 'uploaded_by');
+    }
+}
