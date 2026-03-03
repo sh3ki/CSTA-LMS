dcc3a78edf4b7b90f1e1a767f5ebe44befbadeb5
sh3ki <shekaigarcia@gmail.com>
2026-03-03 11:18:18 +0800

    Create csta-lms/app/Models/Task.php

---
+++ csta-lms/app/Models/Task.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = [
+        'subject_id',
+        'title',
+        'description',
+        'due_date',
+        'file_path',
+        'file_name',
+        'total_points',
+        'created_by',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'due_date' => 'datetime',
+        ];
+    }
+
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function submissions()
+    {
+        return $this->hasMany(Submission::class);
+    }
+}
