716f5ced849041836ecdaec839c08b9e5bf385f9
sh3ki <shekaigarcia@gmail.com>
2026-04-06 02:41:41 +0800

    Update csta-lms/app/Models/Subject.php

--- csta-lms/app/Models/Subject.php
+++ csta-lms/app/Models/Subject.php
@@ -6,7 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subject extends Model
 {
-    protected $fillable = ['name', 'class_id', 'description'];
+    protected $fillable = [
+        'name',
+        'subject_code',
+        'course_code',
+        'semester',
+        'class_id',
+        'description',
+        'status',
+        'created_by',
+    ];
+
+    protected static function booted(): void
+    {
+        static::creating(function (Subject $subject) {
+            if (empty($subject->subject_code)) {
+                $subject->subject_code = self::generateUniqueCode();
+            }
+        });
+    }
+
+    public static function generateUniqueCode(): string
+    {
+        do {
+            $code = 'CLS-' . strtoupper(substr(bin2hex(random_bytes(4)), 0, 8));
+        } while (self::where('subject_code', $code)->exists());
+
+        return $code;
+    }
 
     public function schoolClass()
     {
@@ -21,5 +48,10 @@ class Subject extends Model
     public function tasks()
     {
         return $this->hasMany(Task::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
     }
 }
