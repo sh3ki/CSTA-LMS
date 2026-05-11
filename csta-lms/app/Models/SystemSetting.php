3b4bc0b24efa73cced1bb05455a1c8afdc2a1008
sh3ki <shekaigarcia@gmail.com>
2026-05-12 07:52:51 +0800

    Create csta-lms/app/Models/SystemSetting.php

---
+++ csta-lms/app/Models/SystemSetting.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
+
+class SystemSetting extends Model
+{
+    protected $table = 'system_settings';
+
+    protected $fillable = ['key', 'value'];
+
+    public static function get(string $key, mixed $default = null): mixed
+    {
+        $setting = static::where('key', $key)->first();
+        return $setting ? $setting->value : $default;
+    }
+
+    public static function set(string $key, mixed $value): void
+    {
+        static::updateOrCreate(['key' => $key], ['value' => $value]);
+    }
+
+    public static function allSettings(): array
+    {
+        return static::all()->pluck('value', 'key')->toArray();
+    }
+}
