32861694a8c75e6855fb2a75f96e1851261fb972
sh3ki <shekaigarcia@gmail.com>
2026-05-12 07:52:46 +0800

    Update csta-lms/app/Http/Controllers/Student/AnnouncementController.php

--- csta-lms/app/Http/Controllers/Student/AnnouncementController.php
+++ csta-lms/app/Http/Controllers/Student/AnnouncementController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
+use App\Models\Announcement;
+use Illuminate\Http\Request;
 
 class AnnouncementController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('student.announcements.index');
+        $query = Announcement::with('author')
+            ->published()
+            ->forRole('student')
+            ->latest('published_at');
+
+        if ($request->filled('search')) {
+            $s = $request->search;
+            $query->where(function ($q) use ($s) {
+                $q->where('title', 'like', "%$s%")->orWhere('body', 'like', "%$s%");
+            });
+        }
+
+        $announcements = $query->paginate(15)->withQueryString();
+
+        return view('student.announcements.index', compact('announcements'));
     }
 }
